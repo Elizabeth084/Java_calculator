8ab76c3f4c9cfea61eade7b6ac1661acb6df773c
Elizabeth084 <90203242+Elizabeth084@users.noreply.github.com>
2022-04-23 18:30:35 +0300

    Update Main.java

--- practica2/task 1/src/Main.java
+++ practica2/task 1/src/Main.java
@@ -1,50 +1,27 @@
 package com.company;
-
+import java.util.Arrays;
+import java.util.Random;
 public class Main {
-
-
-    private  static  void  printArray(int[]array){
-        for (int i=0;i< array.length;i++){
-            System.out.println(array[i]);
-        }
-    }
-    private static int[] sortVector(int[] vector)
-    {
-        boolean flag;
-        int temp;
-        do {
-            flag = false;
-            for (int i = 0; i < vector.length-1; i++) {
-                if (vector[i] > vector[i+1])
-                {
-                    temp = vector[i];
-                    vector[i] = vector[i+1];
-                    vector[i+1] = temp;
-                    flag = true;
-                }
-            }
-        } while (flag);
-        return vector;
-    }
-    public static void main(String[] args) {
-
-
-        int [] x={3,4,5,2,99,56,34,6,1,457,86,356,56,35,98,54,6,5,3,76,54,5};
-        for(int i=0;i<x.length;i++){
-            if (x[i]==1){
-                int [] t=new int[x.length-i-1];
-                int i1=i;
-                for(int j=0;i1<x.length-1;i1++,j++){
-                    t[j]=x[i1+1];
-
-                }
-                t=sortVector(t);
-                i1=i;
-                for(int j=i1+1,k=0;j<x.length;j++,k++){
-                    x[j]=t[k];
-                }
-                printArray(x);
-            }
-        }
-    }
+private static final int n = 3;
+public static void main(String[] args) {
+int[] x = new Random().ints(n, -99, 99).toArray();
+int[] y = new int[n];
+Arrays.setAll(y, i -> {
+return (x[i] < 0) ? x[i] : 0;
+});
+Arrays.sort(y);
+print_array(x);
+for (int i = 0; i < y.length; i++) {
+if (y[i] < 0) {
+y[i] = x[i] / 2;
+} else{
+y[i] = 0;
+}
+}
+print_array(y);
+}
+private static void print_array(int[] x) {
+Arrays.stream(x).forEach(e -> System.out.printf("% 5d", e));
+System.out.println();
+}
 }

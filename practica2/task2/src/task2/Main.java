08df0e1997c732dc3ff85001d7f03cbb8caae83b
Elizabeth084 <90203242+Elizabeth084@users.noreply.github.com>
2022-04-23 18:31:43 +0300

    Update Main.java

--- practica2/task2/src/task2/Main.java
+++ practica2/task2/src/task2/Main.java
@@ -1,28 +1,15 @@
-package com.company;
-
+import java.util.Random;
 public class Main {
-
-
-    private  static  void  printArray(int[]array){
-        for (int i=0;i< array.length;i++){
-            System.out.println(array[i]);
-        }
-    }
-    public static void main(String[] args) {
-
-        int n=10;
-
-        int [] c=new int[n];
-        int [] x=new int[n];
-        for (int i=0, j=0;i<c.length;i++) {
-            c[i]= (int) (50-Math.random()*100);
-            if (c[i]!=0){
-                x[j]=c[i]*4;
-                j++;
-            }
-        }
-
-        printArray(c);
-        printArray(x);
-    }
+    private static final int n = 5;
+     public static void main(String[] args) {
+     int[] array = new Random().ints(n, -99, 99).toArray();
+     int[] array2 = new int[array.length];
+     int a = 10;
+     for(int i = 0;i < array.length;i++) {
+         if(array[array.length -i - 1]<a) {
+             array2[i]=array[array.length-i-1];
+             System.out.println(array2[i]);
+         }
+     }
+  }
 }
